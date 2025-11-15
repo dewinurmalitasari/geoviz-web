bd7244aada12cf5186ac46f38f4dd6e64d49300b
Muhammad Akbar Reishandy <akbar@reishandy.my.id>
2025-11-16 01:20:10 +0700

    chore: update API base URL to geoviz-server.reishandy.id

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,1 @@
-// export const API_BASE_URL = 'http://127.0.0.1:3000';
-export const API_BASE_URL = 'https://geoviz-server.vercel.app';
+export const API_BASE_URL = 'https://geoviz-server.reishandy.id';
